2deca6b9f36ac89027a5b74ae2dd4f9420924818
marcin p joachimiak <marcin@Fennel.dhcp.lbl.gov>
2017-07-25 15:43:29 -0700

    cleaned up notebook

--- Orange_disease_phenotype_compendium/cluster_phenotype.R
+++ Orange_disease_phenotype_compendium/cluster_phenotype.R
@@ -10,7 +10,7 @@ hamming.distance <- function(x,y){
 }
 
 
-#setwd("~/Documents/VIMSS/ontology/NCATS/notebooks/cq-notebooks/Orange_GO_annotation_compendium/")
+#setwd("~/Documents/VIMSS/ontology/NCATS/notebooks/cq-notebooks/Orange_disease_phenotype_compendium/")
 
 data <- read.csv("human_disease_phenotype_matrix.txt",sep="\t",row.names=1,header=T)
 range <- range(data.matrix(data))
@@ -48,17 +48,16 @@ clusterall <- heatmap(data.matrix(data_great0), distfun = function(x) dist(x, me
 save(clusterall, file="human_disease_phenotype_matrix_great0_clusterall.Rdata")
 
 
-load("human_GO_annotation_matrix_great1_clusterall.Rdata")
+load("human_disease_phenotype_matrix_great1_clusterall.Rdata")
 
 pdf("human_disease_phenotype_matrix_great0_clusterall_100_alll.pdf", height = 20, width = 11)
 eval(clusterall_100_all$call)
 dev.off(2)
 
-png("human_GO_annotation_matrix_great1_hcl.png", height = 1000, width = 800)
+
+png("human_disease_phenotype_matrix_great0.png", height = 20, width = 11)
 eval(clusterall$call)
 dev.off(2)
-
-
 
 ###separate clustering from visualizations
 
@@ -67,21 +66,26 @@ dev.off(2)
 #row_hamming <- outer(t(data.matrix(data)), t(data.matrix(data)), hamming.distance)
 
 ###uses amap hcluster
-hr <- hcluster(t(data.matrix(data)), method="binary", link="complete",nbproc=4)
+hr <- hcluster(t(data.matrix(data_great0)), method="binary", link="complete",nbproc=4)
 save(hr, file="hr_dis_phe.Rdata")
-hc <- hcluster(data.matrix(data), method="binary", link="complete",nbproc=4)
+hc <- hcluster(data.matrix(data_great0), method="binary", link="complete",nbproc=4)
 save(hc, file="hc_dis_phe.Rdata")
 
 #hr <- hclust(as.dist(1-cor(t(y), method="pearson")), method="complete")
 #hc <- hclust(as.dist(1-cor(y, method="spearman")), method="complete")
 
-#cluster_heatmap <- heatmap.2(data.matrix(data), Rowv=as.dendrogram(hr), Colv=as.dendrogram(hc),
-#          scale="none", density.info="density", trace="none")
+cluster_heatmap2 <- heatmap.2(data.matrix(data_great0), Rowv=as.dendrogram(hr), Colv=as.dendrogram(hc),
+         scale="none", density.info="density", trace="none")
+
+cluster_heatmap2 <- heatmap.2(data.matrix(data_great0),
+                              scale="none", density.info="density", trace="none")
+save(cluster_heatmap2, file="human_disease_phenotype_matrix_great0_clusterall_heatmap2.Rdata")
+
 
 cluster_heatmap <- heatmap(data.matrix(data), Rowv=hr, Colv=hc,
                              scale="none", density.info="density", trace="none")
 
-pdf("human_GO_annotation_matrix_great1_hcl.pdf", height = 11, width = 8.5)
+pdf("human_disease_phenotype_matrix_great1_hcl.pdf", height = 11, width = 8.5)
 eval(cluster$call)
 #heatmap(data.matrix(data), Rowv=as.dendrogram(hr), Colv=as.dendrogram(hc),
 #          scale="none", density.info="density", trace="none")
@@ -93,20 +97,18 @@ dev.off(2)
 
 
 clusterall2 <- heatmap.2(data.matrix(data), distfun = function(x) dist(x, method="binary"),  Rowv=hr, Colv=hc, scale="none", col=mypalette)
-save(clusterall2, file="human_GO_annotation_matrix_great1_clusterall2.Rdata")
+save(clusterall2, file="human_disease_phenotype_matrix_great1_clusterall2.Rdata")
 
 
 
 clusterall2 <- heatmap.2(data.matrix(data), distfun = function(x) dist(x, method="binary"),  Rowv=hr, Colv=hc, scale="none", col=mypalette)
-save(clusterall2, file="human_GO_annotation_matrix_great1_clusterall2.Rdata")
+save(clusterall2, file="human_disease_phenotype_matrix_great1_clusterall2.Rdata")
 
 
-save(hr, file="cluster_hr.Rdata")
-save(hc, file="cluster_hc.Rdata")
 save(cluster_heatmap, file="cluster_heatmap.Rdata")
 
 reordered_data <- data.matrix(data)[rev(hr$labels[hr$order]), hc$labels[hc$order]]
-write.table(reordered_data, file="human_GO_annotation_matrix_great1_2DHCLorder.txt")
+write.table(reordered_data, file="human_disease_phenotype_matrix_great1_2DHCLorder.txt")
 
 
 
@@ -116,7 +118,7 @@ write.table(reordered_data, file="human_GO_annotation_matrix_great1_2DHCLorder.t
 
 ###testing
 
-data100 <- read.csv("human_GO_annotation_matrix_great1_part100.txt",sep="\t",row.names=1,header=T)
+data100 <- read.csv("human_disease_phenotype_matrix_great1_part100.txt",sep="\t",row.names=1,header=T)
 row.names(data100) <- data100[,1]
 data100 <- data100[,-1]
 head(data100)

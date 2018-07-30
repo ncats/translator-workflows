b8b6f2d37b0f4f19294955dae97932cb194c9dc9
Tim Putman <timeput@gmail.com>
2018-07-30 16:17:15 -0700

    updated to return as lists or full dataframe

--- GeneStore/gene_store.py
+++ GeneStore/gene_store.py
@@ -2,23 +2,33 @@ import pandas as pd
 
 
 class FanconiGeneImporter(object):
+    sets = {
+        'fa_core': 'FA_1_core_complex.txt',
+    }
     gh_config = {
         "base_url": "https://raw.githubusercontent.com/NCATS-Tangerine/cq-notebooks/master/FA_gene_sets/",
         "columns": ['gene_curie', 'gene_symbol'],
     }
 
-    def __init__(self, gene_set):
-        self.gene_set = gene_set
+    def __init__(self, gene_set_name):
+        self.gene_set_name = gene_set_name
+        self.gene_df = self.gene_set_df()
 
-    def fa_core(self):
+    def gene_set_df(self):
         return pd.read_csv(
             FanconiGeneImporter.gh_config['base_url'] +
-            self.gene_set, sep='\t',
+            FanconiGeneImporter.sets[self.gene_set_name], sep='\t',
             names=FanconiGeneImporter.gh_config['columns']
         )
 
+    def list_gene_ids(self):
+        return self.gene_df['gene_curie'].tolist()
 
+    def list_gene_symbols(self):
+        return self.gene_df['gene_symbol'].tolist()
 
+    def gene_objects(self):
+        return self.gene_df.to_dict(orient='records')
 
 
 

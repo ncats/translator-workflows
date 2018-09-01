c1c53b5ddf9237fb4ab6c87a01204817f429ffff
Tim Putman <timeput@gmail.com>
2018-08-31 23:03:47 -0700

    implemented owlsim phenotype similarity

--- OwlSim3/owlsim_wrapper.py
+++ OwlSim3/owlsim_wrapper.py
@@ -1,12 +1,9 @@
 import requests
-from pprint import pprint
-
 
 class SimSearch(object):
 
     def __init__(self):
         self.sim_endpoint = 'http://owlsim3.monarchinitiative.org/api/'
-        self.staged_results = ''
 
     def phenotype_search(self, phenotype_set, matcher='phenodigm'):
         phenotype_set = SimSearch.filter_bl_phenotypes(phenotype_set)
@@ -17,37 +14,14 @@ class SimSearch(object):
         }
         results = requests.get(url=url, params=params)
         package = results.json()
-        self.staged_results = package
-
-    def disease_results(self):
-        diseases = []
-        curies = []
-        for match in self.staged_results['matches']:
-            if 'NCBIGene' not in match['matchId']:
-                diseases.append(match)
-                curies.append(match['matchId'])
-
-        return {'data': diseases,
-                'disease_curies': curies,
-                }
-
-    def gene_results(self):
-        genes = []
-        curies = []
-        for match in self.staged_results['matches']:
-            if 'NCBIGene' in match['matchId']:
-                genes.append(match)
-                curies.append(match['matchId'])
-
-        return {'data': genes,
-                'disease_curies': curies,
-                }
+        return package
 
     @staticmethod
     def filter_bl_phenotypes(phenotype_list):
         phenotype_blacklist = ['HP:0025023']
         for elem in phenotype_blacklist:
-            phenotype_list.remove(elem)
+            if elem in phenotype_list:
+                phenotype_list.remove(elem)
         return phenotype_list
 
 

a00e57e4b178e192197c1175223852322b34009e
Tim Putman <timeput@gmail.com>
2018-09-13 14:58:48 -0700

    minor changes to support implementation

--- Modules/generic_similarity.py
+++ Modules/generic_similarity.py
@@ -1,20 +1,16 @@
 from ontobio.ontol_factory import OntologyFactory
-from ontobio.assoc_factory import AssociationSetFactory
 from ontobio.io.gafparser import GafParser
 from ontobio.assoc_factory import AssociationSetFactory
 from ontobio.assocmodel import AssociationSet
-from ontobio.ontol import Ontology
 from typing import List, Union, TextIO
-from pprint import pprint
 from ontobio.analysis.semsim import jaccard_similarity
-from MyGene.mygene_client import QueryMyGene
-import numpy as np
+
 
 class GenericSimilarity(object):
     def __init__(self, associations:AssociationSet=None) -> None:
         self.associations = associations
         self.ofactory = OntologyFactory()
-
+        self.scores = []
     def load_associations(self,ont, group, parse=False):
         ont_fac = self.ofactory.create(ont)
         p = GafParser()
@@ -26,17 +22,19 @@ class GenericSimilarity(object):
         assocs = [x for x in assocs if 'header' not in x.keys()]
         self.associations = afactory.create_from_assocs(assocs, ontology=ont_fac)
 
-    def compute_jaccard(self, input_genes:List[dict], lower_bound:float=0.7, upper_bound:float=1.0) -> List[dict]:
+    def compute_jaccard(self, input_genes:List[dict], lower_bound:float=0.7) -> List[dict]:
         similarities = []
         for index, igene in enumerate(input_genes):
             for subject_curie in self.associations.subject_label_map.keys():
-                score = jaccard_similarity(self.associations, igene['uniprot_curie'], subject_curie)
-                if float(score) > float(lower_bound) and float(score) < upper_bound:
-                    subject_label = self.associations.label(subject_curie)
-                    similarities.append({
-                        'input_curie': igene['gene_curie'],
-                        'sim_hit_name': subject_label,
-                        'sim_hit_curie': subject_curie,
-                        'sim_score': score,
-                    })
+                if igene['uniprot_curie'] is not subject_curie:
+                    score = jaccard_similarity(self.associations, igene['uniprot_curie'], subject_curie)
+                    self.scores.append(score)
+                    if float(score) > float(lower_bound):
+                        subject_label = self.associations.label(subject_curie)
+                        similarities.append({
+                            'input_curie': igene['gene_curie'],
+                            'sim_hit_name': subject_label,
+                            'sim_hit_curie': subject_curie,
+                            'sim_score': score,
+                        })
         return similarities

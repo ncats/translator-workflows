a217fe2f652a5fa71399d7c3fdef43f05fac286d
Colin Curtis <ckcurtis@renci.org>
2019-03-13 13:39:43 -0400

    variable re-naming nearly complete

--- WorkFlow9/bicluster_disease_to_phenotype.py
+++ WorkFlow9/bicluster_disease_to_phenotype.py
@@ -9,6 +9,8 @@ bicluster_disease_url = 'https://smartbag-hpotomondo.ncats.io/HPO_to_MONDO_biclu
 # HP is phenotype ... example URL: https://smartbag-hpotomondo.ncats.io/HPO_to_MONDO_hpo/HP%3A0002193/?include_similar=false
 # MONDO is disease ... example URL: https://smartbag-hpotomondo.ncats.io/HPO_to_MONDO_mondo_list/MONDO.0007030/?include_similar=true
 
+# source for diabetes phenotypic features: https://bionames.renci.org/lookup/diabetes/phenotypic%20feature/?include_similar=true
+# diabetes phenotypic features: ['HP:0000819', 'HP:0000873', 'HP:0005978', 'HP:0100651']
 
 class disease_to_phenotype():
     def __init__(self):
@@ -36,18 +38,18 @@ class disease_to_phenotype():
         return curated_ID_list
 
     async def disease_to_phenotype_biclusters_async(self, input_ID_list):
-        bicluster_url_list = [bicluster_gene_url + gene + '/' +'?include_similar=true' for gene in input_ID_list]
+        bicluster_url_list = [bicluster_disease_url + phenotype + '/' +'?include_similar=true' for phenotype in input_ID_list]
         length_bicluster_url_list = len(bicluster_url_list)
         all_biclusters_dict = defaultdict(dict)
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor_1:
-            all_tissues = []
+            all_diseases = []
             loop_1 = asyncio.get_event_loop()
             futures_1 = [ loop_1.run_in_executor(executor_1, requests.get, request_1_url) for request_1_url in bicluster_url_list ]
             for response in await asyncio.gather(*futures_1):
                 response_json = response.json()
                 for x in response_json:
-                    tissues = x['all_col_labels'].split('__')
-                    for y in tissues:
-                        all_tissues.append(y)
-            tissues_counted = Counter(all_tissues)
-        return tissues_counted.most_common()
+                    disease = x['all_col_labels'].split('__')
+                    for y in disease:
+                        all_diseases.append(y)
+            disease_counted = Counter(all_diseases)
+        return disease_counted.most_common()
